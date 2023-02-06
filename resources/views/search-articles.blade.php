b091aae6409d1e7a7453d55d32aba9cb23577cb8
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-02-06 11:50:02 -0300

    fix some labels mismatches in search-articles view

--- resources/views/search-articles.blade.php
+++ resources/views/search-articles.blade.php
@@ -7,17 +7,13 @@
 @stop
 
 @section('content')
-    <div class="container mt-5">
-
-
- 
+    <div class="mt-5"> 
         @foreach($articles as $article)
-            
                 <div class="card mt-2">
                     <div class="card-body">
                         <p><b>Título</b>: <i> {{$article->title}} </i></p>
-                        <p><b>Creado el</b>: {{$article->created_at}}</p>
-                        <p><b>Última modificación</b>: {{$article->draft_last_update}}</p>
+                        <p><b>Fecha de creación </b>: {{$article->created_at}}</p>
+                        <p><b>Última modificación en la versión borrador</b>: {{$article->draft_last_update}}</p>                        
                         <p><b>Estado de publicación</b>: 
                             @if ($article->is_published) 
                                 Publicado en <a href="#">url.com/blog/{{ $article->slug }}</a>
@@ -25,10 +21,12 @@
                                 No publicado
                             @endif
                         </p>
+                        @if ($article->is_published)
+                            <p><b>Última modificación en la versión publicada</b>: {{$article->publication_last_update}}</p>
+                        @endif
                         <div class="d-flex justify-content-end">
                         <a type="button" class="btn btn-primary" href="/admin-panel/articles/edit/{{$article->id}}">Editar artículo</a>
                         <!--x-adminlte-button class="" label="" theme="danger"/-->
-
                         </div>
                     </div>
                 </div>
@@ -38,8 +36,7 @@
     <div class="d-flex justify-content-center">
         {!! $articles->links() !!}
     </div>
-    </div>
-    @stop
+@stop
 
 @section('css')
     
