060a88bb9a2e2bb4dd74ae8cea2d187b164de522
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-02-09 16:21:25 -0300

    add verification in ArticleController methods to allow only the author of an article modify it

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Str;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use DateTime;
 use DateTimeZone;
+use Illuminate\Support\Facades\Auth;
+
 
 
 
@@ -28,25 +30,32 @@ class ArticleController extends Controller
 
     public function edit (Request $request, $id) {
         $article = Article::find($id);
-        return view('article-editor', [
-            'header' =>  'Editar artículo',
-            'title' => $article->title,
-            'content' => $article->draft_content,
-            'articleID' => $id,
-            'slug' => $article->slug,
-            'isPublished'=> $article->is_published,
-            'draft_last_update' => $article->draft_last_update,
-            'publication_last_update' => $article->publication_last_update
-        ]);
+        $loggedInUserID = Auth::user()->id;
+        $articleUserID = $article->user->id;
+        if ($article && ($loggedInUserID == $articleUserID)){
+            return view('article-editor', [
+                'header' =>  'Editar artículo',
+                'title' => $article->title,
+                'content' => $article->draft_content,
+                'articleID' => $id,
+                'slug' => $article->slug,
+                'isPublished'=> $article->is_published,
+                'draft_last_update' => $article->draft_last_update,
+                'publication_last_update' => $article->publication_last_update
+            ]);
+        } else{
+            //return 404 page
+        }
     }
 
     public function save (Request $request, $id) {
-        $publish = $request->boolean('publish');
         $article = Article::find($id);
+        $publish = $request->boolean('publish');
+        $loggedInUser = Auth::user();
         $date = now();
         $newTitle = $request->title == null ? 'Sin título' : $request->title;
         $newContent = $request->draft_content == null ? '' : $request->content;
-        if($article != null){
+        if($article && ($article->user->id == $loggedInUser->id)){ 
             if($article->title != $newTitle) {
                 $article->title = $newTitle;
                 $article->slug = SlugService::createSlug(Article::class, 'slug', $newTitle);
@@ -57,35 +66,41 @@ class ArticleController extends Controller
             }
             $article->save();
         } else {
-            Article::create([
-                'id' => $id,
-                'title' => $newTitle,
-                'draft_content' => $newContent,
-                'draft_last_update' => $date
-            ]);
+            $article = new Article;
+            $article->id = $id;
+            $article->title = $newTitle;
+            $article->draft_content = $newContent;
+            $article->draft_last_update = $date;
+            $article->user()->associate($loggedInUser);
+            $article->save();
         }
         if($publish){
-            Article::where('id', $id)
-                ->update([
-                    'is_published' => true,
-                    'publication_last_update' => $date,     
-                    'publication_content' =>  $newContent
-                ]);
+            $article->is_published = true;
+            $article->publication_last_update = $date;     
+            $article->publication_content = $newContent;
+            $article->save();
         }    
         return response()->noContent();
     }
 
     public function unpublish (Request $request, $id) {
         $article = Article::find($id);
-        $article->is_published = false;
-        $article->save();
-        return redirect()->route('edit', [
-            'id' => $id
-        ]); 
+        $loggedInUserID = Auth::user()->id;
+        $articleUserID = $article->user->id;
+        $date = now();
+        if($article && ($loggedInUserID == $articleUserID)){
+            $article->is_published = false;
+            $article->save();
+            return redirect()->route('edit', [
+                'id' => $id
+            ]); 
+        } else {
+            //return 404 error response
+        }
     }
 
     public function loadSearchArticlesView(){
-        $articles = Article::orderBy('created_at', 'DESC')->paginate(5);
+        $articles = Article::where('user_id', Auth::user()->id)->orderBy('created_at', 'DESC')->paginate(5);
         return view('search-articles', compact('articles'));
     }
 
