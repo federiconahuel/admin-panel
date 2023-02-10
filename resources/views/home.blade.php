3cbd48198d62716b1685e672dba0383921d8a258
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-02-10 08:35:55 -0300

    add direct access links and change page title in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,46 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Home')
 
 @section('content_header')
-    <h1>Bienvenido</h1>
+    <h1>Accesos directos</h1>
 @stop
 
 @section('content')
-    <p></p>
+    <div class="mt-4">
+        <div class="row">
+            <div class="col-md-6 mb-2">
+            <x-adminlte-card theme="primary" >
+                <div class="d-flex justify-content-between">
+                    <h5>
+                        <i class="fas fa-plus" aria-hidden="true"></i>
+                        Crear artículo
+                    </h5>
+                    <a href="{{  route('create-article')  }}">
+                        Abrir editor de artículos
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>                        
+                </div>
+                </x-adminlte-card>
+            </div>
+            <div class="col-md-6 mb-2">
+            <x-adminlte-card theme="primary" >
+                <div class="d-flex justify-content-between">
+                    <h5>
+                        <i class="fas fa-edit" aria-hidden="true"></i>
+                        Modificar artículo
+                    </h5>
+                    <a href="{{  route('search-articles')  }}">
+                        Seleccionar artículo
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </x-adminlte-card>
+            </div>
+        </div>
+    </div>
+
+
 @stop
 
 @section('css')
